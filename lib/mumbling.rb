3ab580e0ef91f5e82b4cb675d2fc7a3f1ce40216
Chloe <chloe.wong@madetech.com>
2020-09-20 22:47:09 +0100

    refactor to put output at top of function

--- lib/mumbling.rb
+++ lib/mumbling.rb
@@ -14,13 +14,13 @@
 class Mumbling
     # Returns true for a palindrome, false otherwise.
     def mumble_letters(input)
+        output = ""
         if input.length == 1
-            return input.upcase
+            return input[0].upcase
         end
         if input.length == 2
-            output = ""
-            output << "#{input[0].upcase}-"
-            output << "#{input[1].upcase}#{input[1].downcase}"
+            output << input[0].upcase
+            output << "-#{input[1].upcase}#{input[1].downcase}"
         end
     end
   end
