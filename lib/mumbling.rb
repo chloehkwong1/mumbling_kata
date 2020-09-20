9daeeb95ebb04d86eaec0e26b868d448967acab3
Chloe <chloe.wong@madetech.com>
2020-09-20 20:15:23 +0100

    only variable changes

--- lib/mumbling.rb
+++ lib/mumbling.rb
@@ -1,7 +1,19 @@
+# class Mumbling
+#     attr_accessor :input
+
+#     def initialize(input)
+#         @input = input
+#     end
+
+#     def input
+#         self.downcase
+#     end
+
+# end
+
 class Mumbling
-
-    def test(input)
-    input.upcase
+    # Returns true for a palindrome, false otherwise.
+    def one_letter_input(input)
+        input.upcase
     end
-
-end
+  end
--- spec/mumbling_spec.rb
+++ spec/mumbling_spec.rb
@@ -2,10 +2,10 @@ require_relative "../lib/mumbling"
 
 describe "1 letter tests" do
     it "returns a capitalised 'A' when input is 'a'" do
-        expect(Mumbling.new.test('a')).to eq('A')
+        expect(Mumbling.new.one_letter_input('a')).to eq('A')
     end
 
     it "returns a capitalised 'B' when input is 'b'" do
-        expect(Mumbling.new.test('b')).to eq('B')
+        expect(Mumbling.new.one_letter_input('b')).to eq('B')
     end
 end
