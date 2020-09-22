fbb84b137ec11d83ea4adebf31ec8e8216438388
Chloe <chloe.wong@madetech.com>
2020-09-22 21:18:17 +0100

    removed comments, code complete

--- lib/mumbling.rb
+++ lib/mumbling.rb
@@ -1,5 +1,4 @@
 class Mumbling
-    # Returns true for a palindrome, false otherwise.
     def self.mumble_letters(input)
         output = input.split("").map.with_index do |letter, index| 
             "#{letter.upcase}#{letter.downcase * index}"
